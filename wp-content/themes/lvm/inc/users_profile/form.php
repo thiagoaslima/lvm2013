322b9620c29fb3abc10eb6697234f05008842b0d
thiagoaslima <thiagoaslima@gmail.com>
2013-05-19 20:04:54 -0300

    echo meta data with isset confirmation

--- wp-content/themes/lvm/inc/users_profile/form.php
+++ wp-content/themes/lvm/inc/users_profile/form.php
@@ -9,7 +9,7 @@
             </th>
 
             <td>
-                <input type="text" name="user-login" id="user-login" readonly value=<?php echo $user_info->user_login ?> class="regular-text">
+                <input type="text" name="user-login" id="user-login" readonly value="<?php echo $user_info->user_login ?>" class="regular-text">
             </td>
         </tr>
 
@@ -19,7 +19,7 @@
             </th>
 
             <td>
-                <input type="text" name="nickname" id="nickname" value=<?php echo $user_meta['nickname'][0] ?> class="regular-text"><br>
+                <input type="text" name="nickname" id="nickname" value="<?php echo $user_meta['nickname'][0]; ?>" class="regular-text"><br>
                 <span class="description"><?php _e('Preencha este campo com outro valor caso você queira que o Wordpress lhe chame por outro nome que não o nome de login', 'lvm-lang'); ?></span>
             </td>
         </tr>
@@ -48,7 +48,7 @@
             </th>
 
             <td>
-                <input type="text" name="first_name" id="first_name" value=<?php echo $user_meta['first_name'][0] ?> class="regular-text">
+                <input type="text" name="first_name" id="first_name" value="<?php echo $user_meta['first_name'][0]; ?>" class="regular-text">
             </td>
         </tr>
 
@@ -58,7 +58,7 @@
             </th>
 
             <td>
-                <input type="text" name="last_name" id="last_name" value=<?php echo $user_meta['last_name'][0] ?> class="regular-text">
+                <input type="text" name="last_name" id="last_name" value="<?php echo $user_meta['last_name'][0] ?>" class="regular-text">
             </td>
         </tr>
 
@@ -68,8 +68,19 @@
             </th>
 
             <td>
-                <input type="text" name="nascimento" id="nascimento" value="<?php echo esc_attr( get_the_author_meta( 'nascimento', $user->ID ) ); ?>" class="regular-text" /><br />
+                <input type="text" name="nascimento" id="nascimento" value="<?php echo isset($user_meta['nascimento']) ? $user_meta['nascimento'][0] : ''; ?>" class="regular-text" /><br />
                 <span class="description"><?php _e('Por favor, preencha com seu nascimento', 'lvm-lang') ?></span>
+            </td>
+        </tr>
+
+        <tr>
+            <th>
+                <label for="description"><?php _e('Texto de apresentação', 'lvm-lang') ?></label>
+            </th>
+
+            <td>
+                <textarea name="description" id="description" rows="5" cols="30"><?php echo isset($user_meta['description']) ? $user_meta['description'][0] : ''); ?></textarea>
+                <br /><span class="description"><?php _e('Por favor, preencha com seu nascimento', 'lvm-lang') ?></span>
             </td>
         </tr>
     </table>
